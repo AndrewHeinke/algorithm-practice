6c7ae8bf85a6b543465020b79f265bebbb93f27a
Andrew <andrew.j.heinke@gmail.com>
2016-09-07 00:29:52 -0500

    truncate strings and chunking arrays

---
+++ truncateString.js
@@ -0,0 +1,15 @@
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  if (str.length > num && num > 3) {
+    str = str.slice(0, (num - 3)) + "...";
+    return str;
+  } else if (str.length > num && num <= 3) {
+
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+}
+
+truncateString("Absolutely Longer", 2);
