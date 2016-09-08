ec57df0c75be245baa8b20847b216679d3c98b7b
Andrew <andrew.j.heinke@gmail.com>
2016-09-07 23:58:56 -0500

    more practice algorithms added to practice js

--- practice.js
+++ practice.js
@@ -1,16 +1,40 @@
-// function reverseString(str) {
-//   console.log("String Before: " + str);
-//   str = str.toString().split('').reverse().join('');
-//   console.log("Reversed String: " + str);
-// }
-function reverseString(str) {
-  var reversedString = "";
+// reverse a string without using the javascript reverse method
+
+function stringReverse(str) {
   str = str.toString().split('');
+  var newString = "";
   for (var i = str.length - 1; i >= 0; i--) {
-    reversedString += str[i];
+    newString += str[i];
   }
-  console.log("Reversed String: " + reversedString);
+  console.log(newString);
 }
 
+stringReverse("Old McDonald had a farm");
+console.log("<><><><><><><><><><><><><><><><><><><><>");
 
-reverseString("Big Pappi");
+// Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+function repeatString(str, num) {
+  if (num < 1) {
+    return "";
+  }
+  var word = str;
+  for (var i = 0; i < num; i++) {
+    str += word;
+  }
+  console.log(str);
+}
+
+repeatString("bruh", 10);
+console.log("<><><><><><><><><><><><><><><><><><><><>");
+
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].substring(1);
+  }
+  str = str.join(' ');
+  console.log(str);
+}
+
+titleCase("old mcDonAlD hAD A fARm lil BRUH");
